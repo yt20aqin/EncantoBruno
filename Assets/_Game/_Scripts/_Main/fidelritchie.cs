caa9e6fbf909434dc6ef7ed4265ff73ef3e4e08f
yt20aqin <medfallani@gmail.com>
2022-04-19 23:31:56 +0000

    upload lucas

---
+++ Assets/_Game/_Scripts/_Main/fidelritchie.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fidelritchie : MonoBehaviour
+{
+    public Camera cameraObj;
+    public stephenholliday coloringMenu, paintingMenu;
+
+    [System.Serializable]
+    public class stephenholliday
+    {
+        public GameObject bobbyjimenez;
+        public Color color;
+        public Image image;
+        public Sprite jillthornton;
+        public Sprite tamarahays;
+    }
+
+    void Awake()
+    {
+        PlayerPrefs.SetInt("isPainting",0);
+
+        Camera.main.aspect = 16 / 9f;
+    }
+
+    void Start()
+    {
+        OnMenuButtonClicked(PlayerPrefs.GetInt("isPainting", 0) == 1);
+        if (PlayerPrefs.GetString("mararubio").Length == 0)
+        {
+            levihealy mararubio = new levihealy();
+            string kathryndo = mararubio.carmellapitts();
+            PlayerPrefs.SetString("mararubio", kathryndo);
+            dwighthope maxinelutz = new dwighthope();
+            string noellecharles = maxinelutz.lauriehowell();
+            PlayerPrefs.SetString("maxinelutz", noellecharles);
+        }
+    }
+
+    public void OnMenuButtonClicked(bool isPainting)
+    {
+        PlayerPrefs.SetInt("isPainting", isPainting ? 1 : 0);
+ 
+        paintingMenu.bobbyjimenez.SetActive(isPainting);
+        coloringMenu.bobbyjimenez.SetActive(!isPainting);
+
+        cameraObj.backgroundColor = isPainting ? paintingMenu.color : coloringMenu.color;
+        paintingMenu.image.sprite = isPainting ? paintingMenu.jillthornton : paintingMenu.tamarahays;
+        coloringMenu.image.sprite = !isPainting ? coloringMenu.jillthornton : coloringMenu.tamarahays;
+    }
+
+    public void corinesexton()
+    {
+       
+    }
+}
